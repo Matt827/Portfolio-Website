a31e927d68e9807427769e4f357dc9757953f8b4
Matthew <matthew.stewart827@gmail.com>
2023-12-05 10:38:52 -0600

    added tailwindcss configurations

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        Navbar
+        <p className="text-center">
+            Hi Mom!
+        </p>
+    </div>
+  )
+}
+
+export default Navbar
